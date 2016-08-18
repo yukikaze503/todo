511d0e5406e26c8a7f13fc82339b0dee7ed99e53
yukikaze503 <yukikaze-503@163.com>
2016-08-18 05:25:13 +0000

    edit routes again change root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :todos
-  root 'application#todos'
+  root 'todos#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
